1f78986f5b89fd77c515b11746ad78a9d0490e59
halysson <halyssonfreitas@gmail.com>
2022-01-18 13:31:35 -0300

    #update courses.controller.ts to #add findAll() findOne() create() methods draft

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Param, Post, Res } from '@nestjs/common';
 
 @Controller('courses')
-export class CoursesController {}
+export class CoursesController {
+    @Get()
+    findAll(@Res() response) {
+        return response.status(200).send(`Listagem de cursos`);
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id:string){
+        return `Curso #${id}`;
+    }
+
+    @Post()
+    @HttpCode(HttpStatus.NO_CONTENT)
+    create(@Body('name') name): string{
+        return name;
+    }
+}
