0fea6bcc87ce4ce7bc8396945a90a64eeb8f3998
halysson <halyssonfreitas@gmail.com>
2022-01-18 13:54:04 -0300

    #update - courses.controller getting dependency injection by contructor method from courses.service

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,7 +1,11 @@
 import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post, Res } from '@nestjs/common';
+import { CoursesService } from './courses.service';
 
 @Controller('courses')
 export class CoursesController {
+
+    constructor(private readonly coursesService: CoursesService){}
+
     @Get()
     findAll(@Res() response) {
         return response.status(200).send(`Listagem de cursos`);
